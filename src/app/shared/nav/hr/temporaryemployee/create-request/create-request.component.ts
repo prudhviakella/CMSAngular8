9901b7e57e06be79fad63937f222427ee021f037
prudhviakella <prudhvi.akella@abylle.in>
2019-11-06 21:28:16 +0530

    first commit

--- src/app/shared/nav/hr/temporaryemployee/create-request/create-request.component.ts
+++ src/app/shared/nav/hr/temporaryemployee/create-request/create-request.component.ts
@@ -14,12 +14,12 @@ export class CreateRequestComponent implements OnInit {
     this.tmpemployeecreate = new FormGroup({
       role: new FormControl('', Validators.required),
       name: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required,Validators.email]),
-      phoneext: new FormControl('', [Validators.required,Validators.pattern(/^\+[0-9]+$/)]),
-      phonenumber: new FormControl('', [Validators.required,Validators.pattern(/^[6-9]\d{9}$/)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phoneext: new FormControl('', [Validators.required, Validators.pattern(/^\+[0-9]+$/)]),
+      phonenumber: new FormControl('', [Validators.required, Validators.pattern(/^[6-9]\d{9}$/)]),
       currentdesgination: new FormControl('', Validators.required),
       currentcompany: new FormControl('', Validators.required),
-      adhar: new FormControl('', Validators.required),
+      adhar: new FormControl('', [Validators.required, Validators.pattern(/^\d{12}$/)]),
     });
   }
 
